d89e5e2b0053bb0a1ad179c53653c695877d6ba1
Ali Mohammad Hayajneh <hayajneh030@gmail.com>
2026-01-22 19:16:41 +0300

    Include all sub-projects and Dockerfiles

--- angular/angularProject/src/app/model/paginated.result.ts
+++
@@ -1,8 +0,0 @@
-export class PaginatedResult<T>{
-    count!: number;  // Assuming every model will have an id
-    data!: T;
-
-    constructor(init?: Partial<PaginatedResult<T>>) {
-        Object.assign(this, init);
-    }
-}
